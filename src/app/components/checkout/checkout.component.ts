c1b863989ac087028c03d63fb4eedcb26fef9e3b
nphungthinh2601 <nguyenphuchungthinhcongviec@gmail.com>
2022-01-15 09:36:14 +0700

    update

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
 import {
   FormBuilder,
   FormControl,
@@ -11,26 +12,30 @@ import { CheckoutService } from '../../services/checkout.service';
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CheckoutComponent implements OnInit {
   items = this.checkoutService.getItems();
   loading = false;
   submitted = false;
-
+  isAgree = false;
   formGroup = this.formBuilder.group({
     firstName: new FormControl('', [
       Validators.required,
-      Validators.minLength(5),
       Validators.maxLength(50),
     ]),
     lastName: new FormControl('', [
       Validators.required,
-      Validators.minLength(5),
+      Validators.minLength(1),
       Validators.maxLength(50),
     ]),
     phoneNumber: new FormControl('', [
       Validators.required,
-      Validators.maxLength(10),
+      Validators.pattern('^[0-9]{10}$'),
+    ]),
+    address: new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
     ]),
   });
 
@@ -41,7 +46,9 @@ export class CheckoutComponent implements OnInit {
   test(event: any): void {
     this.submitted = true;
   }
-
+  ClickCheckbox(): void {
+    this.isAgree = this.isAgree ? false : true;
+  }
   onSubmit(): void {
     this.submitted = true;
     this.items = this.checkoutService.clearCart();
