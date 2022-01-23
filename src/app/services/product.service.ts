e1a0600890b3004fe036f84e82871f68315e4758
Mr.Trinh <trinhxuanvy1@gmail.com>
2022-01-23 12:22:00 +0700

    update rating

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -19,14 +19,17 @@ export class ProductService {
     return this.http.get<Product[]>(this.apiURL);
   }
 
-  getProductByStoreID(id:string): Observable<Product[]>{
-    return this.http.get<Product[]>(this.apiURL+`/store/`+`${id}`)
+  getProductByStoreID(id: string): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiURL + `/store/` + `${id}`);
   }
-  getProductsByName(name: string): Observable<Product[]>{
-    return this.http.get<Product[]>(this.apiURL+`/find/${name}`);
+  getProductsByName(name: string): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiURL + `/find/${name}`);
   }
-  geEssentialProduct():Observable<Product[]>{
-    return this.http.get<Product[]>(this.apiURL+`/essential`);
+  geEssentialProduct(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiURL + `/essential`);
+  }
 
+  getProductById2(id: string): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiURL + `/${id}`);
   }
 }
