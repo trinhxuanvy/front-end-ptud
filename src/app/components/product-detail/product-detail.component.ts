6e28f7b634842ad92687f32f993a6e4d8c8d2ec4
18120580 <quangtho991@gmail.com>
2022-01-25 10:12:50 +0700

    update

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -3,6 +3,8 @@ import { ProductService } from '../../services/product.service';
 import { Product } from '../../interfaces/interfaces';
 import { ActivatedRoute } from '@angular/router';
 import { InvoiceDetailService } from '../../services/invoice-detail.service';
+import { CustomerService } from 'src/app/services/customer.service';
+import { AuthService } from 'src/app/share/auth/auth.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -11,19 +13,30 @@ import { InvoiceDetailService } from '../../services/invoice-detail.service';
 })
 export class ProductDetailComponent implements OnInit {
   product!: Product;
-
+  customerID='';
+  currentUser: any;
   constructor(
     private productService: ProductService,
     private activeRoute: ActivatedRoute,
-    private invoiceDetailService: InvoiceDetailService
+    private invoiceDetailService: InvoiceDetailService,
+    private customerService: CustomerService,
+    private auth: AuthService
   ) {}
 
   ngOnInit(): void {
+    this.currentUser = this.auth.getUser();
+    this.customerID = this.currentUser.id;
     this.activeRoute.params.subscribe((id) => {
       let productId = id['id'].toString();
       this.productService.getProductById2(productId).subscribe((data) => {
         this.product = data[0];
       });
     });
+  }
+  onInsertCart(proid:String){
+    if(this.currentUser!=null)
+    this.customerService.insertProductToCart(proid,this.customerID).subscribe((data)=>{
+      console.log("đã thêm vào giỏ hàng");
+    })
   }
 }
--- src/app/components/upload-product/upload-product.component.ts
+++ src/app/components/upload-product/upload-product.component.ts
@@ -2,16 +2,15 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import { HttpHeaders } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http'
+import { ActivatedRoute } from '@angular/router';
 import { AuthService } from 'src/app/share/auth/auth.service';
 import {
   Store,
   Product,
-  ProductOfStore,
 } from '../../interfaces/interfaces';
 import { StoreService } from '../../services/store.service';
 import { ProductService } from '../../services/product.service';
 import { FormControl, FormGroup } from '@angular/forms';
-import { isDefined } from '@ng-bootstrap/ng-bootstrap/util/util';
 
 @Component({
   selector: 'app-upload-product',
@@ -24,14 +23,16 @@ export class UploadProductComponent implements OnInit {
     file:new FormControl(),
   });
   customerID='';
+  storeID='';
+  store:any;
   currentUser: any;
-  store: Store | undefined;
   listProduct: Product[] = [];
   constructor(
     private http: HttpClient,
     private productService: ProductService,
     private storeService: StoreService,
-    private auth: AuthService
+    private auth: AuthService,
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
@@ -39,11 +40,11 @@ export class UploadProductComponent implements OnInit {
     this.customerID = this.currentUser.id;
   }
   onImport(){
-    this.geStoreByOwner();
-    if(this.store){
+    this.storeID = this.activatedRoute.snapshot.params['id'];
+    if(this.storeID){
     let formdata=new FormData();
     formdata.append('upload', this.fileInput.nativeElement.files[0]);
-    this.productService.upLoadProductsByExcel(formdata,this.store.id).subscribe((data)=>{
+    this.productService.upLoadProductsByExcel(formdata,this.storeID).subscribe((data)=>{
       this.listProduct=data;
     })
   }
